081d9a19832d854bb7eeb4a873a05aa4997f3833
kataday <yosuke.katada0718@gmail.com>
2017-05-31 13:31:53 +0900

    Calling Bot test2.

--- messages/bot.js
+++ messages/bot.js
@@ -1,40 +1,22 @@
-// "use strict";
-// const useEmulator = (process.env.NODE_ENV == 'development');
-// const builder = require("botbuilder");
-// const botbuilder_azure = require("botbuilder-azure");
-// const connector = useEmulator
-//   ? new builder.ChatConnector()
-//   : new botbuilder_azure.BotServiceConnector({
-//     appId: process.env['MicrosoftAppId'],
-//     appPassword: process.env['MicrosoftAppPassword'],
-//     stateEndpoint: process.env['BotStateEndpoint'],
-//     openIdMetadata: process.env['BotOpenIdMetadata']
-//   });
-// const path = require('path');
-//
-// const bot = new builder.UniversalBot(connector);
-// bot.localePath(path.join(__dirname, './locale'));
-//
-// bot.dialog('/', function(session) {
-//   session.send('You said ' + session.message.text + '!!!');
-// });
-//
-// module.exports = bot;
-
-
-var calling = require('botbuilder-calling');
-
-// Create calling bot
-var connector = new calling.CallConnector({
-    callbackUrl: 'https://localhost:3978/api/calls',
+"use strict";
+const useEmulator = (process.env.NODE_ENV == 'development');
+const builder = require("botbuilder");
+const botbuilder_azure = require("botbuilder-azure");
+const connector = useEmulator
+  ? new builder.ChatConnector()
+  : new botbuilder_azure.BotServiceConnector({
     appId: process.env['MicrosoftAppId'],
-    appPassword: process.env['MicrosoftAppPassword']
-});
-var bot = new calling.UniversalCallBot(connector);
+    appPassword: process.env['MicrosoftAppPassword'],
+    stateEndpoint: process.env['BotStateEndpoint'],
+    openIdMetadata: process.env['BotOpenIdMetadata']
+  });
+const path = require('path');
 
-// Add root dialog
-bot.dialog('/', function (session) {
-    session.send('Watson... come here!');
+const bot = new builder.UniversalBot(connector);
+bot.localePath(path.join(__dirname, './locale'));
+
+bot.dialog('/', function(session) {
+  session.send('You said ' + session.message.text + '!!!');
 });
 
 module.exports = bot;
--- messages/index.js
+++ messages/index.js
@@ -1,18 +1,42 @@
-"use strict";
-// require('dotenv').config();
-const useEmulator = (process.env.NODE_ENV == 'development');
-const bot = require('./bot.js');
+// "use strict";
+// // require('dotenv').config();
+// const useEmulator = (process.env.NODE_ENV == 'development');
+// const bot = require('./bot.js');
+//
+// if (useEmulator) {
+//   const restify = require('restify');
+//   const server = restify.createServer();
+//   server.listen(3978, function() {
+//     console.log('bot endpont at http://localhost:3978/api/messages');
+//   });
+//   // server.post('/api/messages', bot.connector('*').listen());
+//   server.post('/api/calls', bot.connector.listen());
+// } else {
+//   module.exports = {
+//     default: bot.connector('*').listen()
+//   }
+// }
 
-if (useEmulator) {
-  const restify = require('restify');
-  const server = restify.createServer();
-  server.listen(3978, function() {
-    console.log('bot endpont at http://localhost:3978/api/messages');
-  });
-  // server.post('/api/messages', bot.connector('*').listen());
-  server.post('/api/calls', bot.connector.listen());
-} else {
-  module.exports = {
-    default: bot.connector('*').listen()
-  }
-}
+
+var restify = require('restify');
+var calling = require('botbuilder-calling');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create calling bot
+var connector = new calling.CallConnector({
+    callbackUrl: 'https://localhost:3978/api/calls',
+    appId: process.env['MicrosoftAppId'],
+    appPassword: process.env['MicrosoftAppPassword']
+});
+var bot = new calling.UniversalCallBot(connector);
+server.post('/api/calls', connector.listen());
+
+// Add root dialog
+bot.dialog('/', function (session) {
+    session.send('Watson... come here!');
+});
