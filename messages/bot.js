9e3aa4f071506e497b0463dc77f3a55b4a62ddef
kataday <yosuke.katada0718@gmail.com>
2017-05-31 15:06:24 +0900

    card test.

--- messages/bot.js
+++ messages/bot.js
@@ -17,12 +17,14 @@ bot.localePath(path.join(__dirname, './locale'));
 
 bot.dialog('/', [(session, args, next) => {
     const card = new builder.ThumbnailCard(session);
-    card.buttons([new builder.CardAction(session).title('Add a number').value('Add').type('imBack'), new builder.CardAction(session).title('Get help').value('Help').type('imBack')]).text(`What would you like to do?`);
+    card.buttons([
+      new builder.CardAction(session).title('有給休暇').value('paidVacation').type('imBack')
+    ]).text(`どの休暇を申請しますか？`);
 
     const message = new builder.Message(session);
     message.addAttachment(card);
 
-    session.send(`Hi there! I'm the calculator bot! I can add numbers for you.`);
+    session.send(`こんにちは。どの休暇申請のお手伝いをさせて頂きます。`);
     // we can end the conversation here
     // the buttons will provide the appropriate message
     session.endConversation(message);
