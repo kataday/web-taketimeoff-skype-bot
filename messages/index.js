162a1827a9ed4f1a22e93971a7d6889e07c9e80a
kataday <yosuke.katada0718@gmail.com>
2017-05-31 14:31:37 +0900

    Calling Bot 一旦諦める。

--- messages/index.js
+++ messages/index.js
@@ -1,46 +1,46 @@
-// "use strict";
-// // require('dotenv').config();
-// const useEmulator = (process.env.NODE_ENV == 'development');
-// const bot = require('./bot.js');
-//
-// if (useEmulator) {
-//   const restify = require('restify');
-//   const server = restify.createServer();
-//   server.listen(3978, function() {
-//     console.log('bot endpont at http://localhost:3978/api/messages');
-//   });
-//   // server.post('/api/messages', bot.connector('*').listen());
-//   server.post('/api/calls', bot.connector.listen());
-// } else {
-//   module.exports = {
-//     default: bot.connector('*').listen()
-//   }
-// }
+"use strict";
+// require('dotenv').config();
+const useEmulator = (process.env.NODE_ENV == 'development');
+const bot = require('./bot.js');
 
-
-// var restify = require('restify');
-var calling = require('botbuilder-calling');
-
-// Setup Restify Server
-// var server = restify.createServer();
-// server.listen(process.env.port || process.env.PORT || 3978, function () {
-//    console.log('%s listening to %s', server.name, server.url);
-// });
-
-// Create calling bot
-var connector = new calling.CallConnector({
-    callbackUrl: 'https://web-taketimeoff.azurewebsites.net/api/calls?code=lR3d4p4tS9zpmuNEr7urorZAmLGgi/euuVajgiJABmDdAYC0N9ddjw==',
-    appId: process.env['MicrosoftAppId'],
-    appPassword: process.env['MicrosoftAppPassword']
-});
-var bot = new calling.UniversalCallBot(connector);
-// server.post('/api/calls', connector.listen());
-
-// Add root dialog
-bot.dialog('/', function (session) {
-    session.send('Watson... come here!');
-});
-
-module.exports = {
-  default: connector.listen()
+if (useEmulator) {
+  const restify = require('restify');
+  const server = restify.createServer();
+  server.listen(3978, function() {
+    console.log('bot endpont at http://localhost:3978/api/messages');
+  });
+  // server.post('/api/messages', bot.connector('*').listen());
+  server.post('/api/calls', bot.connector.listen());
+} else {
+  module.exports = {
+    default: bot.connector('*').listen()
+  }
 }
+
+
+// // var restify = require('restify');
+// var calling = require('botbuilder-calling');
+//
+// // Setup Restify Server
+// // var server = restify.createServer();
+// // server.listen(process.env.port || process.env.PORT || 3978, function () {
+// //    console.log('%s listening to %s', server.name, server.url);
+// // });
+//
+// // Create calling bot
+// var connector = new calling.CallConnector({
+//     callbackUrl: 'https://web-taketimeoff.azurewebsites.net/api/calls?code=lR3d4p4tS9zpmuNEr7urorZAmLGgi/euuVajgiJABmDdAYC0N9ddjw==',
+//     appId: process.env['MicrosoftAppId'],
+//     appPassword: process.env['MicrosoftAppPassword']
+// });
+// var bot = new calling.UniversalCallBot(connector);
+// // server.post('/api/calls', connector.listen());
+//
+// // Add root dialog
+// bot.dialog('/', function (session) {
+//     session.send('Watson... come here!');
+// });
+//
+// module.exports = {
+//   default: connector.listen()
+// }
