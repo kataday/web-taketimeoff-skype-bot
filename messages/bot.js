7a0d203b9f2f851b44b98dceba941959ebc61f96
kataday <yosuke.katada0718@gmail.com>
2017-05-31 16:41:29 +0900

    HeroCard test.

--- messages/bot.js
+++ messages/bot.js
@@ -16,29 +16,30 @@ const bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
 
 bot.dialog('/', [(session, args, next) => {
-    // const card = new builder.ThumbnailCard(session);
-    // card.buttons([
-    //   new builder.CardAction(session).title('有給休暇').value('paidVacation').type('imBack')
-    // ]).text(`どの休暇を申請しますか？`);
-    //
-    // const message = new builder.Message(session);
-    // message.addAttachment(card);
+    const card = new builder.HeroCard(session).images(null)
+    card.buttons([
+      new builder.CardAction(session).title('有給休暇').value('paidVacation').type('imBack')
+    ]).text(`どの休暇を申請しますか？`);
 
-    // session.send(`こんにちは。休暇申請のお手伝いをさせて頂きます。`);
+    const message = new builder.Message(session);
+    message.addAttachment(card);
+
+    session.send(`こんにちは。休暇申請のお手伝いをさせて頂きます。`);
     // we can end the conversation here
     // the buttons will provide the appropriate message
-    // session.endConversation(message);
+    session.endConversation(message);
     // builder.Prompts.choice(session, 'どの休暇を申請しますか？', ['有給休暇'], {
     //   maxRetries: 3,
     //   retryPrompt: 'Ooops, what you wrote is not a valid option, please try again'
     // });
 
-    var msg = new builder.Message(session).text("Thank you for expressing interest in our premium golf shirt! What color of shirt would you like?").suggestedActions(builder.SuggestedActions.create(session, [
-      builder.CardAction.imBack(session, "productId=1&color=green", "Green"),
-      builder.CardAction.imBack(session, "productId=1&color=blue", "Blue"),
-      builder.CardAction.imBack(session, "productId=1&color=red", "Red")
-    ]));
-    session.send(msg);
+    // var msg = new builder.Message(session).text("Thank you for expressing interest in our premium golf shirt! What color of shirt would you like?").suggestedActions(builder.SuggestedActions.create(session, [
+    //   builder.CardAction.imBack(session, "productId=1&color=green", "Green"),
+    //   builder.CardAction.imBack(session, "productId=1&color=blue", "Blue"),
+    //   builder.CardAction.imBack(session, "productId=1&color=red", "Red"),
+    //   new builder.CardAction(session).title('有給休暇').value('paidVacation').type('imBack')
+    // ]));
+    // session.send(msg);
 
 
   }
