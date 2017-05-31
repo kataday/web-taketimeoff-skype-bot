f1ac747773e8deb1c8392b92d2bcde86a269d802
kataday <yosuke.katada0718@gmail.com>
2017-05-31 14:53:29 +0900

    カード表示テスト。

--- messages/bot.js
+++ messages/bot.js
@@ -15,8 +15,18 @@ const path = require('path');
 const bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
 
-bot.dialog('/', function(session) {
-  session.send('You said ' + session.message.text + '!!!');
-});
+bot.dialog('/', [(session, args, next) => {
+    const card = new builder.ThumbnailCard(session);
+    card.buttons([new builder.CardAction(session).title('Add a number').value('Add').type('imBack'), new builder.CardAction(session).title('Get help').value('Help').type('imBack')]).text(`What would you like to do?`);
+
+    const message = new builder.Message(session);
+    message.addAttachment(card);
+
+    session.send(`Hi there! I'm the calculator bot! I can add numbers for you.`);
+    // we can end the conversation here
+    // the buttons will provide the appropriate message
+    session.endConversation(message);
+  }
+]);
 
 module.exports = bot;
