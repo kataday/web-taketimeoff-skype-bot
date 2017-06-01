66bb1f1b04a129a4c6397ce3f55e78f0e2aee194
kataday <yosuke.katada0718@gmail.com>
2017-06-01 17:36:11 +0900

    日付を聞き出す処理を追加。 改行テスト。

--- messages/bot.js
+++ messages/bot.js
@@ -18,30 +18,77 @@ bot.localePath(path.join(__dirname, './locale'));
 bot.dialog('/', [(session, args, next) => {
     const card = new builder.HeroCard(session).images(null);
     card.buttons([
-      new builder.CardAction(session).title('有給休暇').value('paidVacation').type('imBack')
+      new builder.CardAction(session).title('有給休暇').value('有給休暇').type('imBack')
     ]).text(`どの休暇を申請しますか？`);
 
     const message = new builder.Message(session);
     message.addAttachment(card);
 
-    session.send(`こんにちは。休暇申請のお手伝いをさせて頂きます。`);
-    // we can end the conversation here
-    // the buttons will provide the appropriate message
+    session.send(`こんにちは。休暇申請のお手伝いをさせて頂きます。\n\nあああ`);
     session.endConversation(message);
-    // builder.Prompts.choice(session, 'どの休暇を申請しますか？', ['有給休暇'], {
-    //   maxRetries: 3,
-    //   retryPrompt: 'Ooops, what you wrote is not a valid option, please try again'
-    // });
+  }
+]);
 
-    // var msg = new builder.Message(session).text("Thank you for expressing interest in our premium golf shirt! What color of shirt would you like?").suggestedActions(builder.SuggestedActions.create(session, [
-    //   builder.CardAction.imBack(session, "productId=1&color=green", "Green"),
-    //   builder.CardAction.imBack(session, "productId=1&color=blue", "Blue"),
-    //   builder.CardAction.imBack(session, "productId=1&color=red", "Red"),
-    //   new builder.CardAction(session).title('有給休暇').value('paidVacation').type('imBack')
-    // ]));
-    // session.send(msg);
+bot.dialog('有給休暇', [
+  (session, args, next) => {
+    session.send(`有給休暇申請ですね。取得する日を教えてください。`);
+    session.beginDialog('/ensureDate');
+  },
+  (session, result) => {
+    session.send(`${result.takeDate}ですね。`);
 
+    // session.endConversation(`${result.takeDate}ですね。`);
+  }
+])
+.triggerAction({matches: /^有給休暇/i})
+.cancelAction('CancelTakePaidVacation', '中止しました。', {
+  matches: /^cancel$/,
+  onSelectAction: (session, args) => {
+    session.endConversation(`中止しました。`);
+  },
+  confirmPrompt: `本当に中止しても良いですか？`
+})
+// .beginDialogAction('Total', 'Total', {matches: /^total$/})
+// .beginDialogAction('HelpAddNumber', 'Help', {
+//   matches: /^help$/,
+//   dialogArgs: {
+//     action: 'AddNumber'
+//   }
+// });
 
+bot.dialog('/ensureDate', [
+  (session, args, next) => {
+    if (!session.privateConversationData.year) {
+      builder.Prompts.number(session, `何年ですか？`, {maxRetries: 3});
+    } else {
+      next();
+    }
+  },
+  (session, results, next) => {
+    if (results.response) {
+      session.privateConversationData.year = results.response;
+    }
+    if (!session.privateConversationData.month) {
+      builder.Prompts.number(session, `何月ですか？`, {maxRetries: 3});
+    } else {
+      next();
+    }
+  },
+  (session, results, next) => {
+    if (results.response) {
+      session.privateConversationData.month = results.response;
+    }
+    if (!session.privateConversationData.day) {
+      builder.Prompts.number(session, `何日ですか？`, {maxRetries: 3});
+    } else {
+      next();
+    }
+  },
+  (session, results, next) => {
+    if (results.response) {
+      session.privateConversationData.day = results.response;
+    }
+    session.endDialogWithResult({ takeDate: `${session.privateConversationData.year}年${session.privateConversationData.month}月${session.privateConversationData.day}日` });
   }
 ]);
 
